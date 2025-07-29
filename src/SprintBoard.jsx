9b39348ed9705a6bb43e4cde7f580d91a225a43e
Sugaam-dev <sugaam.dev@gmail.com>
2025-07-29 16:37:45 +0530

    Update SprintBoard.jsx

--- src/SprintBoard.jsx
+++ src/SprintBoard.jsx
@@ -1408,7 +1408,7 @@ const SprintBoard = () => {
       }
 
       // Fetch specific sprint stories from API
-      const response = await fetch(`https://sprint-backend-73ho.onrender.com/${sprintInfo.id}`, {
+      const response = await fetch(`https://sprint-backend-73ho.onrender.com/stories/${sprintInfo.id}`, {
       // const response = await fetch(`http://127.0.0.1:8000/stories/${sprintInfo.id}`, {
         
         method: 'GET',
